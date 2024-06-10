c2eb2e7d122496398905379f690f2f7059cd2556
Achraf IGHID <31323958+achrafyo@users.noreply.github.com>
2024-06-10 12:58:24 +0100

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -11,7 +11,7 @@
       mm = String(today.getMonth() + 1).padStart(2, "0"),
       yyyy = today.getFullYear(),
       nextYear = yyyy + 1,
-      dayMonth = "06/10/",
+      dayMonth = "12/10/",
       birthday = dayMonth + yyyy;
   
   today = mm + "/" + dd + "/" + yyyy;
